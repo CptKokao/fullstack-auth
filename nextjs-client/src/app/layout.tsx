31d0690e0e2f82a16f3caebca2d5a96bdcdedce5
cpt.kokao <cpt.kokao@gmail.com>
2025-11-10 13:03:55 +0300

    Добавляет клиентскую часть (NextJs)

---
+++ nextjs-client/src/app/layout.tsx
@@ -0,0 +1,33 @@
+import type { Metadata } from "next";
+import { GeistSans } from "geist/font/sans";
+import "@/shared/styles/globals.css";
+
+export const metadata: Metadata = {
+	title: {
+		absolute: 'Курс по авторизации',
+		template: '%s | Курс по авторизации'
+	},
+	description:
+		'Это учебный проект, созданный для демонстрации полного цикла авторизации пользователей'
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+  		<html lang='en'>
+			<body className={GeistSans.variable}>
+				{/* <MainProvider> */}
+					<div className='relative flex min-h-screen flex-col'>
+						{/* <ToggleTheme /> */}
+						<div className='flex h-screen w-full items-center justify-center px-4'>
+							{children}
+						</div>
+					</div>
+				{/* </MainProvider> */}
+			</body>
+		</html>
+  );
+}
